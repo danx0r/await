a6b018083a21b0f2a29b0163e77026e5b5c547ad
danx0r <dan.miller@eye0.com>
2021-07-09 18:18:13 -0700

    true concurrence

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
-import asyncio
+import time, asyncio
 
 
 async def task_func():
     print('in task_func')
     await asyncio.sleep(3)
+    print ("func done")
     return 'the result'
 
 
 async def main(loop):
-#    print('creating task')
-#    task = asyncio.create_task(task_func())
-#    print('waiting for {!r}'.format(task))
-    return_value = await task_func()
-#    print('task completed {!r}'.format(task))
+    print('creating task')
+    task = asyncio.create_task(task_func())
+    print('waiting for {!r}'.format(task))
+    for i in range(5):
+        print (i)
+        await asyncio.sleep(1)
+    return_value = await task
+    print('task completed {!r}'.format(task))
     print('return value: {!r}'.format(return_value))
 
 
